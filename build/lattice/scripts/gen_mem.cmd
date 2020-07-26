8ddd2223c45aa6ead0192bac8e91e2c08316bc09
PulseRainmaker <pulserain@pulserain.com>
2020-07-26 01:01:04 -0700

    add scripts to help generating memory initial files

--- build/lattice/scripts/gen_mem.cmd
+++
@@ -1,9 +0,0 @@
-rem scripts to generate memory init files from hex file 
-rem example: gen_mem.cmd C:\Users\pulse\AppData\Local\Temp\arduino_build_32247\sketch_jul24a.ino.hex
-
-python ./gen_mem.py %1 0 > ..\MachXO3D_Breakout\breakout\mem_init_0.mem
-python ./gen_mem.py %1 1 > ..\MachXO3D_Breakout\breakout\mem_init_1.mem
-python ./gen_mem.py %1 2 > ..\MachXO3D_Breakout\breakout\mem_init_2.mem
-python ./gen_mem.py %1 3 > ..\MachXO3D_Breakout\breakout\mem_init_3.mem
-
-
